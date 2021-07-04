e6da37ab6161e1e995a977ad3faf31b2b9fc7a32
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-07-04 16:26:55 -0400

    add Tag

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import { CreateUserController } from './controllers/CreateUserController'
+import { CreateTagController } from './controllers/CreateTagController'
+import { ensureAdmin } from './middlewares/ensureadmin'
 
 const router = Router()
 
 const createUserController = new CreateUserController()
+const createTagController = new CreateTagController()
 
 router.post('/users', createUserController.create)
+router.post('/tags', ensureAdmin, createTagController.create)
 
 export { router }
